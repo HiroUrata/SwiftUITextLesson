422ecfb54970fcd21dd33553d80b5c3abc094d9f
HiroUrata <1712026@i.softbank.jp>
2021-10-14 19:14:04 +0900

    VStack 練習追加

--- SwiftUITextLesson/ContentView.swift
+++ SwiftUITextLesson/ContentView.swift
@@ -9,8 +9,14 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        VStack{
+            Text("VStack01Text").padding()
+            Text("VStack02Text").padding()
+            Text("VStack03Text").padding()
+                
+        }
+        
     }
 }
 
